45fa0b9913ba3ce28e8df81dd8357494e186057a
aborgra <audrey.borgra@gmail.com>
2020-05-06 15:54:30 -0500

    added recipe model

--- SmartPantry/Models/Recipe.cs
+++ SmartPantry/Models/Recipe.cs
@@ -1,11 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SmartPantry.Models
 {
+    public class Response
+    {
+        [JsonPropertyName("hits")]
+        public List<Recipe> Hits { get; set; }
+    }
     public class Recipe
     {
+        [JsonPropertyName("label")]
+        public string Label { get; set; }
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+        [JsonPropertyName("ingredients")]
+        public List<Ingredient> Ingredients { get; set; }
+        
+        [JsonPropertyName("calories")]
+        public double Calories { get; set; }
+        [JsonPropertyName("totalTime")]
+        public decimal TotalTime { get; set; }
+        [JsonPropertyName("totalNutrients")]
+        public TotalNutrient TotalNutrients { get; set; }
     }
+
+    public class Ingredient
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+        [JsonPropertyName("weight")]
+        public decimal Weight { get; set; }
+    }
+
+    public class TotalNutrient
+    {
+        [JsonPropertyName("Fat")]
+        public Fat Fat { get; set; }
+        [JsonPropertyName("CHOCDF")]
+        public Carbs Carbs { get; set; }
+        [JsonPropertyName("FIBTG")]
+        public Fiber Fiber { get; set; }
+        [JsonPropertyName("SUGAR")]
+        public Sugar Sugar { get; set; }
+        [JsonPropertyName("PROCNT")]
+        public Protein Protein { get; set; }
+        [JsonPropertyName("CHOLE")]
+        public Cholesterol Cholesterol { get; set; }
+        [JsonPropertyName("NA")]
+        public Sodium Sodium { get; set; }
+    }
+
+    public class Nutrient
+    {
+        [JsonPropertyName("label")]
+        public string Label { get; set; }
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+        [JsonPropertyName("unit")]
+        public string Unit { get; set; }
+    }
+
+    public class Fat : Nutrient { }
+    public class Carbs : Nutrient { }
+    public class Fiber : Nutrient { }
+    public class Sugar : Nutrient { }
+    public class Protein : Nutrient { }
+    public class Cholesterol : Nutrient { }
+    public class Sodium : Nutrient { }
 }
