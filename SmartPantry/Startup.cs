be204bbb39a1fe620453436a8bfef692dce3f7d7
Audrey Borgra <audrey.borgra@gmail.com>
2020-05-25 22:27:56 -0500

    formatted index quantities

--- SmartPantry/Startup.cs
+++ SmartPantry/Startup.cs
@@ -28,7 +28,7 @@ namespace SmartPantry
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(this.Configuration.GetConnectionString("SmartPantryConnStr")));
             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
