e937662709d74578706a92b93f4725c7ed4acfb0
Audrey Borgra <audrey.borgra@gmail.com>
2020-06-05 21:27:40 -0500

    changed API and connStr values

--- SmartPantry/Controllers/RecipesController.cs
+++ SmartPantry/Controllers/RecipesController.cs
@@ -34,8 +34,7 @@ namespace SmartPantry.Controllers
 
             var apiId = configuration["RecipeAPIIdentifier"];
             var apiKey = configuration["RecipeAPIKey"];
-            var uri = $"https://api.edamam.com/search?q={query}&to=50&app_id=824953da&app_key=231a6fa597253d411d38714f22311a5b";
-            //var uri = $"https://api.edamam.com/search?q={query}&to=50&app_id={apiId}&app_key={apiKey}";
+            var uri = $"https://api.edamam.com/search?q={query}&to=50&app_id={apiId}&app_key={apiKey}";
             var client = new HttpClient();
 
             // Set request header to accept JSON
